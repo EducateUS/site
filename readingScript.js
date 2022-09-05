aa4d9edf11e2b407c99493e239543effebd64874
auldzk <auldzk@guilford.edu>
2022-09-05 17:43:02 -0400

    java script collapse round 2

--- readingScript.js
+++ readingScript.js
@@ -1,3 +1,5 @@
+collapsetext = null;
+
 thetitle = document.getElementById("hidden");
 
 thetitle.addEventListener("click", onClick);
@@ -5,14 +7,14 @@ thetitle.addEventListener("click", onClick);
 thetext = document.getElementById("hiddentext");
 
 function onClick() {
-    thetext.classList.remove("dontshow");
-    thetitle.innerHTML = 'Information and Ideas <div id="collapse">(collapse)</div>';
+    thetext.classList.toggle("dontshow");
+    thetitle.innerHTML = 'Information and Ideas <span id="collapse"><em>(collapse)</em></span>';
+    collapsetext = document.getElementById("collapse");
+
+    collapsetext.addEventListener("click", collapseClick);
 }
 
-collapsetext = document.getElementById("collapse");
-
-collapsetext.addEventListener("click", collapseClick);
-
 function collapseClick() {
-    thetext.classList.add("dontshow");
+    thetext.classList.toggle("dontshow");
+    thetitle.innerHTML = '';
 }
